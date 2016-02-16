75adc47ea5652b0f95242402465e0562fa336c86
Alyson <emerald.aly@gmail.com>
2016-02-15 22:46:48 -0500

    changed PORT to be consistent

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var port = 3000;
+var PORT = 3000;
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
