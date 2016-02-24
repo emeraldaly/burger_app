a1fc9731fa6b3848a003524686e0aa1a3911e682
Alyson <emerald.aly@gmail.com>
2016-02-23 20:04:06 -0500

    changed connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,5 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'burger_app_db'
-})
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
     if (err) {
